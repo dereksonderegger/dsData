0316481a8048eaaf8f471fce873dbb0eba722310
Derek Sonderegger <derek.sonderegger@nau.edu>
2015-10-15 16:25:40 -0700

    Added Polishing Data

---
+++ R/Polishing.R
@@ -0,0 +1,15 @@
+#' Manufacturing data on how long it takes to polish a dish 
+#' 
+#' This data describes the final step in processing dishes
+#' before the are packaged and sold. The polishing time could
+#' depend on the size of the dish, the price that it will be sold
+#' at, and the type of dish.
+#' @format A data frame with 58 rows and 4 variables:
+#' \describe{
+#'   \item{Diameter}{The diameter of the dish.}
+#'   \item{Time}{The amount of time necessary to polish the dish.}
+#'   \item{Price}{How much the dish will sell for.}
+#'   \item{Type}{The type of dish: Casserole, Bowl, Dish, Tray, or Plate}
+#' }
+"Dishes"
+
