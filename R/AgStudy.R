828be27949298b5b70c6809505bddf482cb80f48
Derek Sonderegger <derek.sonderegger@gmail.com>
2016-10-15 00:03:19 -0700

    Fixing AgData

--- R/AgStudy.R
+++ R/AgStudy.R
@@ -5,25 +5,21 @@
 #' Within each plot, we have four sub-plots where we can apply a fertilizer 
 #' treatment and we have two fertilizer treatments (low/high).  Within each 
 #' sub-plot we have five replicate samples.
-#' 
 #' @format A data.frame with 96 observations
 #' \describe{
-#'     \item plot Which plot the observation comes from
-#'     \item subplot Which subplot the observation comes from
-#'     \item rep The replicate number within subplot
-#'     \item Irrigation The irrigation level of the plot
-#'     \item Fertilizer The fertilizer level of the subplot
-#' } 
+#'     \item{plot}{Which plot the observation comes from}
+#'     \item{subplot}{Which subplot the observation comes from}
+#'     \item{rep}{The replicate number within subplot}
+#'     \item{Irrigation}{The irrigation level of the plot}
+#'     \item{Fertilizer}{The fertilizer level of the subplot}} 
 #' @examples
 #' ggplot(AgData, aes(x=Fertilizer, y=yield, color=Irrigation)) + 
 #'   geom_point() +
 #'   facet_wrap( ~ plot, ncol=4, labeller=label_both)
-#' 
-"AgStudy"
-
+"AgData"
 
 # set.seed(345)
-# data <- expand.grid(plot=factor(1:8), subplot=factor(1:4), rep=factor(1:5)) %>% 
+# data <- expand.grid(plot=factor(1:8), subplot=factor(1:4), rep=factor(1:5)) %>%
 #   arrange(plot, subplot, rep) %>%
 #   cbind( Irrigation = rep(0:1, each=16*5),
 #          Fertilizer = rep(0:1, times=16, each=5)) %>%
@@ -32,11 +28,11 @@
 # X <- model.matrix( ~ Irrigation + Fertilizer, data=data)
 # beta <- c(20, 3, 5)
 # 
-# y <- X %*% beta + 
+# y <- X %*% beta +
 #   model.matrix(~-1+plot,data=data) %*% rnorm(8, sd=3)  +
 #   model.matrix(~-1+plot:subplot, data=data) %*% rnorm(8*4, sd=2) +
 #   model.matrix(~-1+plot:subplot:rep, data=data) %*% rnorm(8*4*5, sd=1)
 # data$yield <- as.vector(y)
 # 
 # AgData <- data
-# save(AgData, file='../data/AgData.rdata')
+# save(AgData, file='data/AgData.rdata')
